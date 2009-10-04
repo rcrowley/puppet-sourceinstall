d1fd6c22130ecf35a3c3c33761921c8231b162c3
Richard Crowley <r@rcrowley.org>
2009-10-04 19:50:53 +0000

    Rewritten sourceinstall to fork out so Ruby memory usage doesn't get out of control.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,58 +1,18 @@
 class sourceinstall::build {
 	package { "build-essential": ensure => present }
-}
-
-define sourceinstall($package, $version, $tarball, $flags, $bin) {
-	include sourceinstall::build
-
-	file { "/root/$package-$version.tar.bz2":
-		source => "$tarball",
+	file { "/usr/local/bin/sourceinstall":
+		source => "puppet://$servername/sourceinstall/sourceinstall",
 		ensure => present,
 	}
-	exec { "extract-$package-$version":
-		require => File["/root/$package-$version.tar.bz2"],
-		cwd => "/root",
-		command => "tar xf /root/$package-$version.tar.bz2",
-		creates => "/root/$package-$version",
-		unless => "test -d /opt/$package-$version",
-	}
+}
 
-	exec { "configure-$package-$version":
+define sourceinstall($tarball, $prefix, $flags) {
+	include sourceinstall::build
+	exec { "/usr/local/bin/sourceinstall $tarball $prefix $flags >>/tmp/sourceinstall.out 2>>/tmp/sourceinstall.err":
 		require => [
 			Package["build-essential"],
-			Exec["extract-$package-$version"]
+			File["/usr/local/bin/sourceinstall"]
 		],
-		cwd => "/root/$package-$version",
-		command => "./configure --prefix=/opt/$package-$version $flags",
 		timeout => "-1",
-		creates => "/root/$package-$version/Makefile",
-		onlyif => "test -d /root/$package-$version",
 	}
-	exec { "make-$package-$version":
-		require => [
-			Package["build-essential"],
-			Exec["configure-$package-$version"]
-		],
-		cwd => "/root/$package-$version",
-		command => "make",
-		timeout => "-1",
-		creates => "/root/$package-$version/$bin",
-		onlyif => "test -d /root/$package-$version",
-	}
-	exec { "install-$package-$version":
-		require => [
-			Package["build-essential"],
-			Exec["make-$package-$version"]
-		],
-		cwd => "/root/$package-$version",
-		command => "make install",
-		creates => "/opt/$package-$version",
-		onlyif => "test -d /root/$package-$version",
-	}
-
-	exec { "remove-$package-$version":
-		require => Exec["install-$package-$version"],
-		command => "rm -rf /root/$package-$version*",
-	}
-
 }
